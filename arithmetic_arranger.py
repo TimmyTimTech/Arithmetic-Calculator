7be4fac4e4b42d32bb1e8241de9d27e7c10666d1
TimmyTimTech <namathetim3@gmail.com>
2023-11-06 12:23:56 +0200

    commit on 2023/11/06

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,7 +1,15 @@
-question = input("Enter the question: ")
-try:
-    integer_number = int(question)
-except ValueError:
-    print("The cannot be converted to an integer.")
+#prompt user for input.
+user_input = input("Enter the equation :")
+#convert string into array using the split().
+array_input = user_input.split()
+def arithmetic_arranger(array_input):
+    #convert the array strings into integer values.
+    result = []
+    for int_list in array_input:
+        try:
+            result.append(int(int_list))
+        except ValueError:
+            print("Inavlid number format.")
+    print(result)
 
-print(integer_number)
+arithmetic_arranger(array_input)
